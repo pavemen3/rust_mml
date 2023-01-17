8d7756bc17ef0d75024cf6c3df70ad0a11c74f4d
Keisuke Matsuda <matsuda@wisesips.com>
2023-01-17 22:09:22 +0900

    mml

--- src/wav_writer.rs
+++ src/wav_writer.rs
@@ -27,11 +27,36 @@ pub fn write(filename: &str, notes: Vec<Note>, bpm: f32) {
     let len = (4.0 / note.len as f32 * (60.0 / bpm) * SAMPLE_RATE) as u32;
     // 周波数を計算
     let tone = if note.no >= 0 {
-      440,0 * 2.0f32.powf((note.no -69) as f32 / 12.0)
+      440.0 * 2.0f32.powf((note.no - 69) as f32 / 12.0)
     } else { 0.0 };
     write_tone(&mut fw, tone, len);
   }
 }
 
 // サイン波をファイルに書き込む
+fn write_tone<W>(fw: &mut WavWriter<W>, tone: f32, len: u32)
+where W: Write + Seek {
+  for t in 0..len {
+    let a = t as f32 / SAMPLE_RATE;
+    let v = (a * tone * 2.0 * PI).sin();
+    fw.write_sample((v * i16::MAX as f32) as i16).unwrap();
+  }
+}
+
+// ライブラリーをテストする
+#[cfg(test)]
+mod wav_writer_test {
+  use super::*; // 外側の要素を利用
+  #[test]
+  fn notes_test() {
+    // ノートの一覧を作成
+    let notes: Vec<Note> = vec![
+      Note{ no: 60, len: 4},
+      Note{ no: 62, len: 4},
+      Note{ no: 64, len: 4},
+    ];
+    // WAVファイルへ保存
+    write("test.wav", notes, 120.0);
+  }
+}
 
